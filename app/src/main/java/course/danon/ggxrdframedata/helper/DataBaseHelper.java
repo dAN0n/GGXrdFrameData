f9bab0652f0056732096b28c1279ec85deb71bd9
d@N0n <dan0na@ya.ru>
2015-05-01 18:34:52 +0300

    Some minor replacement of Base.close() calls (Fixed crash)

--- app/src/main/java/course/danon/ggxrdframedata/helper/DataBaseHelper.java
+++ app/src/main/java/course/danon/ggxrdframedata/helper/DataBaseHelper.java
@@ -13,10 +13,13 @@ public class DataBaseHelper extends SQLiteAssetHelper {
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        db = getReadableDatabase();
     }
 
+    private void open(){
+        db = getReadableDatabase();
+    }
     public int getRowCount(String TableName){
+        open();
         String Selection = "Max(_id)";
         Cursor c = db.query(TableName, new String[] {Selection}, null, null, null, null, null);
         c.moveToFirst();
@@ -24,21 +27,25 @@ public class DataBaseHelper extends SQLiteAssetHelper {
     }
 
     public Cursor getColumn(String TableName, String Selection){
+        open();
         return db.query(TableName, new String[]{Selection}, null, null, null, null, null);
     }
 
     public Cursor getPortraitTable(String TableName){
+        open();
         String Selection[] = new String[]{"_id", "Input", "Guard", "Startup", "`Frame Adv.` as Adv", "Type"};
         return db.query(TableName, Selection, null, null, null, null, null);
     }
 
     public Cursor getLandscapeTable(String TableName){
+        open();
         String Selection[] = new String[]{"_id", "Input", "Damage", "Tension", "RISC", "Prorate", "`Attack Lv.` as Attack",
             "Guard", "Cancel", "RC", "Startup", "Active", "Recovery", "`Frame Adv.` as Adv", "`Inv.` as Inv", "Type"};
         return db.query(TableName, Selection, null, null, null, null, null);
     }
 
 /*    public Cursor getRow(String TableName, String id){
+        open();
         String Selection[] = new String[]{"Input", "Damage", "Tension", "RISC", "Prorate", "`Attack Lv.` as Attack",
             "Guard", "Cancel", "RC", "Startup", "Active", "Recovery", "`Frame Adv.` as Adv", "`Inv.` as Inv", "Type"};
         String Where = "_id = ?";
@@ -47,6 +54,7 @@ public class DataBaseHelper extends SQLiteAssetHelper {
     }*/
 
     public Cursor getCharInfo(String id){
+        open();
         String TableName = "CharSelectIcons";
         String[] Selection = new String[]{"Char", "FDTableName", "Icon", "`Defense Modifier`", "`Guts Rating`",
             "`Stun Resistance`", "`Jump Startup`", "`Backdash Time`", "`Backdash Invincibility`", "`IK Activation`"};
